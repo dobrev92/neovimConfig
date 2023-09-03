88abc170bdbf460e2d062b942fbfb7eff10e635a
Georgi Dobrev <georgi.dobrev1992@gmail.com>
2023-09-04 02:35:41 +0300

    first commit

---
+++ init.lua
@@ -0,0 +1,9 @@
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
+
+vim.cmd("highlight Normal guibg=none")
+vim.cmd("highlight NormalNC guibg=none")
+vim.cmd("highlight NonText guibg=none")
+
+vim.cmd("highlight NeoTreeNormal guibg=none")
+vim.cmd("highlight NeoTreeNormalNC guibg=none")
